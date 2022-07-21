0e5e15a1dcabfa9d5c16814758bcd0af40bf25c2
powerm1nt <nukaworks@nuka.works>
2022-07-21 22:10:58 +0200

    :sparkles: Updated Button story

--- src/Base/Button/Button.stories.jsx
+++ src/Base/Button/Button.stories.jsx
@@ -1,36 +1,41 @@
 import React from 'react';
 
-import { Button } from './Button';
+import {Button} from './Button';
+import {Application} from "../../Appl/Application/Application";
 
 export default {
-  title: 'Base/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    title: 'Base/Button',
+    component: Button,
+    argTypes: {
+        backgroundColor: {control: 'color'},
+    },
 };
 
-const Template = (args) => <Button {...args} />;
+const Template = (args) =>
+    <Application>
+        <Button {...args} />
+    </Application>;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: 'Button',
+    primary: true,
+    label: 'Button',
+    size: 'small',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+    label: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+    size: 'large',
+    label: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+    size: 'small',
+    label: 'Button',
 };
