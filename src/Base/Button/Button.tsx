e810f339cb8aa3cdad3dc7d773664b14b3753b72
powerm1nt <nukaworks@nuka.works>
2022-07-26 23:11:08 +0200

    :sparkles: Code cleanup

--- src/Base/Button/Button.tsx
+++ src/Base/Button/Button.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes, { InferProps } from 'prop-types'
 import './Button.scss'
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({ primary, backgroundColor, size, label, ...props } :any) => {
+export function Button ({ primary, size, label } :InferProps<typeof Button.propTypes>) {
   const mode = primary ? 'base--button__primary' : 'base--button__secondary'
   return (
     <button
       type="button"
       className={['base--button', `base--button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
     >
       {label}
     </button>
@@ -20,31 +15,13 @@ export const Button = ({ primary, backgroundColor, size, label, ...props } :any)
 }
 
 Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
   primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
   size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * Button contents
-   */
   label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
   onClick: PropTypes.func
 }
 
 Button.defaultProps = {
-  backgroundColor: null,
   primary: false,
-  size: 'small',
-  onClick: undefined
+  size: 'small'
 }
