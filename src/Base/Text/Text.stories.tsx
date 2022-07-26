8e7bc7a7ca602c82276c535ab18c5bffe211c73f
powerm1nt <nukaworks@nuka.works>
2022-07-26 23:02:48 +0200

    :sparkles: ts-ignore any text args

--- src/Base/Text/Text.stories.tsx
+++ src/Base/Text/Text.stories.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-import { Text } from './Text';
+import { Text } from './Text'
 
 export default {
   title: 'Base/Text',
   component: Text
-};
+}
 
-const Template = (args) => <Text {...args} />;
+const Template = (args :any) => <Text {...args} />
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
+// @ts-ignore
 Default.args = {
-  text: "Hello World !"
-};
+  text: 'Hello World !'
+}
