21a0a8635558abb8a1bfec54141568d789c014fb
powerm1nt <nukaworks@nuka.works>
2022-07-27 23:26:58 +0200

    :sparkles: Default theme to light

--- src/Base/Button/Button.tsx
+++ src/Base/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import './Button.scss'
 
-export function Button ({ color, size, label } :InferProps<typeof Button.propTypes>) {
+export function Button ({ color, theme, size, label } :InferProps<typeof Button.propTypes>) {
   return (
     <button
       type="button"
@@ -15,12 +15,14 @@ export function Button ({ color, size, label } :InferProps<typeof Button.propTyp
 
 Button.propTypes = {
   color: PropTypes.oneOf(['default', 'success', 'warning', 'alert']),
+  theme: PropTypes.oneOf(['light', 'dark']),
   size: PropTypes.oneOf(['small', 'medium', 'large']),
   label: PropTypes.string.isRequired,
   onClick: PropTypes.func
 }
 
 Button.defaultProps = {
-  primary: false,
+  color: 'default',
+  theme: 'light',
   size: 'small'
 }
