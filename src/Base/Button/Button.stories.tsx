3d5b95f71ce28f08cc23564892449f66633ae508
powerm1nt <nukaworks@nuka.works>
2022-09-11 19:16:51 +0200

    :truck: Folder update

--- src/Base/Button/Button.stories.tsx
+++ src/Base/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Button } from './Button'
-import { UiApp } from '../../Appl/Layouts/UiApp'
+import { UiApp } from '../../Layouts/UiApp'
 
 export default {
   title: 'Base/Button',
