521c769becf34f7d91db142d894bf8188a1a99b1
powerm1nt <nukaworks@nuka.works>
2022-09-16 23:54:34 +0200

    :sparkles: Bung: Added Config.js

---
+++ büng/src/utils/Config.js
@@ -0,0 +1,9 @@
+// Defines directories & files
+const YAML = require('yaml')
+const fs = require('fs')
+const bungFile = YAML.parse(fs.readFileSync('../../.bung.yml', 'utf-8'))
+const templDir = './templ'
+
+module.exports = {
+  getTemplDir: templDir, getBungFile: bungFile
+}
