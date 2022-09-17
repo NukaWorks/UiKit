8145cea1d0bf234142d6be030ac83ff5bde4d9f9
powerm1nt <nukaworks@nuka.works>
2022-09-17 18:05:09 +0200

    :sparkles: Upper the word

--- büng/src/utils/CompntMgmt.js
+++ büng/src/utils/CompntMgmt.js
@@ -2,11 +2,15 @@ const fs = require('fs')
 const YAML = require('yaml')
 const chalk = require('chalk')
 const { getBungFile, getTemplDir } = require('./Config')
+const { stringUpperFirst } = require('@zerodep/string')
 
 function addComponent (name, category) {
   Object.entries(getBungFile.dirs).forEach(dir => {
     if (dir[0].includes(category)) console.log(dir[1])
   })
+
+  name = stringUpperFirst(name)
+  category = stringUpperFirst(category)
 
   console.log(chalk.cyan(`Creating ${chalk.bold(name)} on ${chalk.bold(category)}...`))
 }
