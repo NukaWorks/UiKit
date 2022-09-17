b5aad4a9bbd8ffe4033f8503145c81124ec515fa
powerm1nt <nukaworks@nuka.works>
2022-09-17 18:03:47 +0200

    :wrench: Updated bung file path

--- büng/src/utils/Config.js
+++ büng/src/utils/Config.js
@@ -1,7 +1,7 @@
 // Defines directories & files
 const YAML = require('yaml')
 const fs = require('fs')
-const bungFile = YAML.parse(fs.readFileSync('../../.bung.yml', 'utf-8'))
+const bungFile = YAML.parse(fs.readFileSync('./.bung.yml', 'utf-8'))
 const templDir = './templ'
 
 module.exports = {
