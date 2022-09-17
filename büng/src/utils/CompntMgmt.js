fa0e4c9e0284808427e6d54588e31634a51b29b1
powerm1nt <nukaworks@nuka.works>
2022-09-18 00:04:18 +0200

    :poop: bung: TODO need to be fixed

--- büng/src/utils/CompntMgmt.js
+++ büng/src/utils/CompntMgmt.js
@@ -5,10 +5,7 @@ const { getBungFile, getTemplDir } = require('./Config')
 const { stringUpperFirst } = require('@zerodep/string')
 
 function addComponent (name, category) {
-  Object.entries(getBungFile.dirs).forEach(dir => {
-    if (dir[0].includes(category)) console.log(dir[1])
-  })
-
+  console.log(searchCategory(category))
   name = stringUpperFirst(name)
   category = stringUpperFirst(category)
 
@@ -17,6 +14,20 @@ function addComponent (name, category) {
 
 function deleteComponent (name, category) {
   console.log(chalk.red(`Deleting ${chalk.bold(name)} on ${chalk.bold(category)}...`))
+}
+
+// TODO: CHANGER STRUCTURE DE DATA, DU GENRE VOIR POUR UNE HASHMAP MAIS EN JS xD
+
+function searchCategory (category) {
+  Object.entries(getBungFile.dirs).forEach(dir => {
+    if (dir[0].includes(category)) {
+      console.log(dir[1])
+      return true
+    } else {
+      console.error(chalk.red(`Category ${category} not found`))
+      return false
+    }
+  })
 }
 
 module.exports = { addComponent, deleteComponent }
