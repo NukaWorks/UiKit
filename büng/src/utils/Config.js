bb60bcf9c293bfa3b56411624bdf1e4a447d2d6e
powerm1nt <nukaworks@nuka.works>
2022-09-20 08:35:59 +0200

    :sparkles: bung: Updated import for templDir

--- büng/src/utils/Config.js
+++ büng/src/utils/Config.js
@@ -2,7 +2,7 @@
 const YAML = require('yaml')
 const fs = require('fs')
 const bungFile = YAML.parse(fs.readFileSync('./.bung.yml', 'utf-8'))
-const templDir = './templ'
+const templDir = './büng/src/templ'
 
 module.exports = {
   getTemplDir: templDir, getBungFile: bungFile
