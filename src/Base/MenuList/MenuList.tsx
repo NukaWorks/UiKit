e5be6143e589e4c6b68fe642bc957c51a04a4539
powerm1nt <nukaworks@nuka.works>
2022-09-27 08:47:04 +0200

    :sparkles: MenuItem: Added items foreach

--- src/Base/MenuList/MenuList.tsx
+++ src/Base/MenuList/MenuList.tsx
@@ -2,15 +2,20 @@ import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import './MenuList.scss'
 
-export function MenuList ({ children, className }: InferProps<typeof MenuList.propTypes>) {
+export function MenuList ({ children, className, items }: InferProps<typeof MenuList.propTypes>) {
+  const list = items.map((item: any) => {
+    return (<div key={item} className={'--ItemContainer'}>{ item }</div>)
+  })
+
   return (
     <div className={['Base--MenuList', className].join(' ')}>
-        Hello World !
+      <>{ list }</>
     </div>
   )
 }
 
 MenuList.propTypes = {
   className: PropTypes.string,
+  items: PropTypes.any,
   children: PropTypes.any
 }
