63f955e0c3460689a4924844ee6abba97d401b91
powerm1nt <nukaworks@nuka.works>
2022-10-02 17:17:06 +0200

    :sparkles: MenuList.stories: Make a new Menu

--- src/Base/MenuList/MenuList.stories.tsx
+++ src/Base/MenuList/MenuList.stories.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
 
 import { MenuList } from './MenuList'
+import { MenuItem } from '../MenuItem/MenuItem'
 
 export default {
   title: 'Base/MenuList',
   component: MenuList
 }
 
-const Template = (args :any) => <MenuList {...args} />
+const Template = (args :any) =>
+    <MenuList {...args}>
+      <MenuItem>About This Mac</MenuItem>
+      <MenuItem>System Preferences...</MenuItem>
+      <MenuItem>App Store...</MenuItem>
+      <MenuItem>---</MenuItem>
+      <MenuItem>Recent Items</MenuItem>
+      <MenuItem>Force Quit...</MenuItem>
+      <MenuItem>---</MenuItem>
+      <MenuItem>Sleep</MenuItem>
+      <MenuItem>Restart...</MenuItem>
+      <MenuItem>Shutdown...</MenuItem>
+      <MenuItem>---</MenuItem>
+      <MenuItem>Lock Screen</MenuItem>
+      <MenuItem>Log Out...</MenuItem>
+    </MenuList>
 
 export const Default = Template.bind({})
 // @ts-ignore
 Default.args = {
-  title: 'DevTools',
-  items: ['Reload', 'Inspect', 'About']
+  title: 'Apple Logo'
 }
