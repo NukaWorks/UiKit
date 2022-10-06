430c436444cf28a9c244c33446edd6299a1b40ff
powerm1nt <nukaworks@nuka.works>
2022-10-06 16:08:29 +0200

    :sparkles: Export MenuBar and Menu

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import { Text } from './Base/Text/Text'
 import { TextField } from './Base/TextField/TextField'
 import { MenuList } from './Base/MenuList/MenuList'
 import { MenuItem } from './Base/MenuItem/MenuItem'
-
+import { MenuBar } from './Base/MenuBar/MenuBar'
+import { Menu } from './Base/Menu/Menu'
 // Application Components
 import { AppHeader } from './Appl/AppHeader/AppHeader'
 
@@ -16,7 +17,9 @@ export {
   Text,
   TextField,
   AppHeader,
+  Menu,
   MenuList,
   MenuItem,
+  MenuBar,
   UiApp
 }
