b620b58fcb5d19cc6f14378155d886b377f337c9
powerm1nt <nukaworks@nuka.works>
2022-10-06 16:09:26 +0200

    :sparkles: Added Menu

---
+++ src/Base/Menu/Menu.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { Menu } from '../Menu/Menu'
+import { MenuItem } from '../MenuItem/MenuItem'
+import { MenuList } from '../MenuList/MenuList'
+
+export default {
+  title: 'Base/Menu',
+  component: Menu
+}
+
+const Template = (args :any) =>
+    <Menu {...args}>
+      <MenuList>
+        <MenuItem>About This Mac</MenuItem>
+        <MenuItem>System Preferences...</MenuItem>
+        <MenuItem>App Store...</MenuItem>
+        <MenuItem>---</MenuItem>
+        <MenuItem>Recent Items</MenuItem>
+        <MenuItem>Force Quit...</MenuItem>
+        <MenuItem>---</MenuItem>
+        <MenuItem>Sleep</MenuItem>
+        <MenuItem>Restart...</MenuItem>
+        <MenuItem>Shutdown...</MenuItem>
+        <MenuItem>---</MenuItem>
+        <MenuItem>Lock Screen</MenuItem>
+        <MenuItem>Log Out...</MenuItem>
+      </MenuList>
+    </Menu>
+
+export const Default = Template.bind({})
+// @ts-ignore
+Default.args = {
+  title: 'Apple Logo'
+}
