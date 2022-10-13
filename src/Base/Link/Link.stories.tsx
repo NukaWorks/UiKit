7bf670cc01d58ea74cc929de314ac03acc789c10
powerm1nt <nukaworks@nuka.works>
2022-10-13 21:26:42 +0200

    :sparkles: Link: Added default values

--- src/Base/Link/Link.stories.tsx
+++ src/Base/Link/Link.stories.tsx
@@ -4,11 +4,16 @@ import { Link } from './Link'
 
 export default {
   title: 'Base/Link',
-  component: Link
+  component: Link,
+  argTypes: {
+    href: { control: 'text' }
+  }
 }
 
-const Template = (args :any) => <Link {...args} href={'#'}>Hello World !</Link>
+const Template = (args :any) => <Link {...args}>Hello World !</Link>
 
 export const Default = Template.bind({})
 // @ts-ignore
-Default.args = {}
+Default.args = {
+  href: 'https://nuka.works/'
+}
