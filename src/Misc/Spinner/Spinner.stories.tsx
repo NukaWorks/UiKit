682c74cb32a31565aa9f15b79cb21f5166981443
FakeSMC.kext <nukaworks@nuka.works>
2022-10-22 17:22:58 +0200

    :sparkles: Spinner.stories: Added custom args

--- src/Misc/Spinner/Spinner.stories.tsx
+++ src/Misc/Spinner/Spinner.stories.tsx
@@ -4,11 +4,31 @@ import { Spinner } from './Spinner'
 
 export default {
   title: 'Misc/Spinner',
-  component: Spinner
+  component: Spinner,
+  argTypes: {
+    color: {
+      options: ['Default', 'Blue'],
+      control: { type: 'select' }
+    },
+    size: {
+      options: ['Small', 'Medium', 'Large'],
+      control: { type: 'select' }
+    }
+  }
 }
 
 const Template = (args :any) => <Spinner {...args} />
 
 export const Default = Template.bind({})
 // @ts-ignore
-Default.args = {}
+Default.args = {
+  size: 'Medium',
+  color: 'Default'
+}
+
+export const Blue = Template.bind({})
+// @ts-ignore
+Blue.args = {
+  size: 'Medium',
+  color: 'Blue'
+}
