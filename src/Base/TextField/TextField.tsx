941d303b574bec3db61c3ebb0aaee62ca7c20c9f
FakeSMC.kext <nukaworks@nuka.works>
2022-10-23 16:10:00 +0200

    :sparkles: Added disabled props

--- src/Base/TextField/TextField.tsx
+++ src/Base/TextField/TextField.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import './TextField.scss'
 
-export function TextField ({ type, placeholder, className } :InferProps<typeof TextField.propTypes>) {
+export function TextField ({ type, placeholder, className, disabled } :InferProps<typeof TextField.propTypes>) {
   return (
     <input
         type={type || ''}
         placeholder={placeholder || ''}
         className={['Base__TextField', className].join(' ')}
+        disabled={disabled || false}
     />
   )
 }
@@ -15,9 +16,11 @@ export function TextField ({ type, placeholder, className } :InferProps<typeof T
 TextField.propTypes = {
   type: PropTypes.oneOf(['text', 'password', 'email', 'number', 'tel', 'url']),
   placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
   className: PropTypes.string
 }
 
 TextField.defaultProps = {
-  type: 'text'
+  type: 'text',
+  disabled: false
 }
