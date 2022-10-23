668af27f636b5b2f2452054a06d333e43e755e6a
FakeSMC.kext <nukaworks@nuka.works>
2022-10-23 18:58:41 +0200

    :construction: ForwardRef component compat

--- src/Base/TextField/TextField.tsx
+++ src/Base/TextField/TextField.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import './TextField.scss'
 
-export function TextField ({ type, placeholder, className, disabled, ...props } :InferProps<typeof TextField.propTypes>) {
+/* export function TextField ({ type, placeholder, className, disabled, ...props } :InferProps<typeof TextField.propTypes>) {
   return (
     <input
         type={type || ''}
@@ -11,7 +11,19 @@ export function TextField ({ type, placeholder, className, disabled, ...props } 
         disabled={disabled || false}
     />
   )
-}
+} */
+
+// eslint-disable-next-line react/display-name
+export const TextField :any = forwardRef((
+  { type, placeholder, className, disabled, ...props } :InferProps<typeof TextField.propTypes>,
+  ref) => (
+    <input
+        type={type || ''}
+        placeholder={placeholder || ''}
+        className={['Base__TextField', className].join(' ')}
+        disabled={disabled || false}
+    />
+))
 
 TextField.propTypes = {
   type: PropTypes.oneOf(['text', 'password', 'email', 'number', 'tel', 'url']),
