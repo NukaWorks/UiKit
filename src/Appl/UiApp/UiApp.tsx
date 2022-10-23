b336b377e297cc1b7e85e0f57f2e4ff76515950d
FakeSMC.kext <nukaworks@nuka.works>
2022-10-23 17:59:42 +0200

    :sparkles: Added ...props pass-trough

--- src/Appl/UiApp/UiApp.tsx
+++ src/Appl/UiApp/UiApp.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import './UiApp.scss'
 
-export function UiApp ({ children, className, rounded }: InferProps<typeof UiApp.propTypes>) {
+export function UiApp ({ children, className, rounded, ...props }: InferProps<typeof UiApp.propTypes>) {
   return (
     <div className={['Appl__UiApp', rounded ? 'Appl__UiApp--Rounded' : '', className].join(' ')}>
       { children }
@@ -13,5 +13,6 @@ export function UiApp ({ children, className, rounded }: InferProps<typeof UiApp
 UiApp.propTypes = {
   className: PropTypes.string,
   rounded: PropTypes.bool,
+  props: PropTypes.any,
   children: PropTypes.any
 }
