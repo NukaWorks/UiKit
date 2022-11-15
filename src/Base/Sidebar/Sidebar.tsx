6c848e9f6fd3e0cb7c6d38f7e7542b17462424ab
Powerm1nt <nukaworks@nuka.works>
2022-11-15 22:28:31 +0100

    :construction: Remove separator

--- src/Base/Sidebar/Sidebar.tsx
+++ src/Base/Sidebar/Sidebar.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import './Sidebar.scss'
 import '../../Assets/Themes/index.scss'
-import {Separator} from "../../Misc/Separator/Separator";
-import {SidebarItem} from "../SidebarItem/SidebarItem";
+import { SidebarItem } from '../SidebarItem/SidebarItem'
 
 export function Sidebar ({ children, className, ...props }: InferProps<typeof Sidebar.propTypes>) {
   return (
@@ -11,9 +10,7 @@ export function Sidebar ({ children, className, ...props }: InferProps<typeof Si
         className={['Base__Sidebar', className].join(' ')}
         {...props}
     >
-      <SidebarItem className="Base__Sidebar--MenuBtn" icon={'menu'}  text={''}/>
-
-      <Separator />
+      <SidebarItem className="Base__Sidebar--MenuBtn" icon={'menu'} text={''}/>
 
       <div className={'Base__Sidebar--Content'}>
         {children || <SidebarItem text={'Sidebar Empty'} icon={'warning'} />}
