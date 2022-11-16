31e53127f90716dc029a33e870f820c346a8a4dc
Powerm1nt <nukaworks@nuka.works>
2022-11-16 21:56:28 +0100

    :sparkles: Impl Active state indicator

--- src/Base/SidebarItem/SidebarItem.tsx
+++ src/Base/SidebarItem/SidebarItem.tsx
@@ -2,33 +2,34 @@ import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import './SidebarItem.scss'
 
-export function SidebarItem ({
-  text,
-  children,
-  active,
-  disableText,
-  icon,
-  className,
-  onClick,
-  ...props
-}: InferProps<typeof SidebarItem.propTypes>) {
+export function SidebarItem (
+  {
+    text,
+    children,
+    active,
+    disableText,
+    icon,
+    className,
+    onClick,
+    ...props
+  }: InferProps<typeof SidebarItem.propTypes>) {
   return (
-    <div
-        className={['Base__SidebarItem', className].join(' ')}
-        onClick={onClick}
-        {...props}
-    >
+      <div
+          className={['Base__SidebarItem', active ? 'Base__SidebarItem--Activated' : '', className].join(' ')}
+          onClick={onClick}
+          {...props}
+      >
 
       <span className="material-symbols-rounded">
         {icon}
       </span>
 
-      {disableText ||
-          (<div className={'Base__SidebarItem--Text'}>
-            {text || children}
-          </div>)
-      }
-    </div>
+        {disableText ||
+            (<div className={'Base__SidebarItem--Text'}>
+              {text || children}
+            </div>)
+        }
+      </div>
   )
 }
 
