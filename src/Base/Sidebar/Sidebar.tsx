cca65a21623a22e1f4012cdfa57c5e57ec64aab9
Powerm1nt <nukaworks@nuka.works>
2022-11-16 18:07:41 +0100

    :sparkles: Impl handleToggle

--- src/Base/Sidebar/Sidebar.tsx
+++ src/Base/Sidebar/Sidebar.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import './Sidebar.scss'
 import '../../Assets/Themes/index.scss'
 import { SidebarItem } from '../SidebarItem/SidebarItem'
 
 export function Sidebar ({ children, className, ...props }: InferProps<typeof Sidebar.propTypes>) {
+  const [close, setClose] = useState(false)
+
+  const handleToggle = () => setClose(!close)
+
   return (
     <div
-        className={['Base__Sidebar', className].join(' ')}
+        className={['Base__Sidebar', close ? 'Base__Sidebar--Closed' : '', className].join(' ')}
         {...props}
     >
-      <SidebarItem className="Base__Sidebar--MenuBtn" icon={'menu'} text={''}/>
+      <SidebarItem className="Base__Sidebar--MenuBtn" icon={'menu'} disableText onClick={() => handleToggle()}/>
 
       <div className={'Base__Sidebar--Content'}>
         {children || <SidebarItem text={'Sidebar Empty'} icon={'warning'} />}
