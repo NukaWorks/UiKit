7b81b83abf68380a8080020ac88ee7db84df53d8
Powerm1nt <nukaworks@nuka.works>
2022-11-16 18:07:25 +0100

    :sparkles: Impl onClick & disableText props

--- src/Base/SidebarItem/SidebarItem.tsx
+++ src/Base/SidebarItem/SidebarItem.tsx
@@ -2,23 +2,32 @@ import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import './SidebarItem.scss'
 
-export function SidebarItem ({ text, icon, className, ...props }: InferProps<typeof SidebarItem.propTypes>) {
+export function SidebarItem ({ text, disableText, icon, className, onClick, ...props }: InferProps<typeof SidebarItem.propTypes>) {
   return (
     <div
         className={['Base__SidebarItem', className].join(' ')}
+        onClick={onClick}
         {...props}
     >
+
       <span className="material-symbols-rounded">
         {icon}
       </span>
-      {text}
+
+      {disableText ||
+          (<div className={'Base__SidebarItem--Text'}>
+            {text}
+          </div>)
+      }
     </div>
   )
 }
 
 SidebarItem.propTypes = {
-  text: PropTypes.string.isRequired,
+  text: PropTypes.string,
   icon: PropTypes.string,
+  disableText: PropTypes.bool,
+  onClick: PropTypes.any,
   className: PropTypes.string,
   props: PropTypes.any
 }
