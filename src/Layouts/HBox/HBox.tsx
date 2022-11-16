7312013fefe40c88c3ee5d663110d521abbc8bac
Powerm1nt <nukaworks@nuka.works>
2022-11-16 16:34:56 +0100

    :sparkles: Impl Box for layouts

--- src/Layouts/HBox/HBox.tsx
+++ src/Layouts/HBox/HBox.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import './HBox.scss'
+import { Box } from '../Box/Box'
 
-export function HBox ({ children, className, ...props }: InferProps<typeof HBox.propTypes>) {
+export function VBox ({ children, className, ...props }: InferProps<typeof VBox.propTypes>) {
   return (
-    <div
-        className={['Layouts__HBox', className].join(' ')}
-        {...props}
-    >
-      {children}
-    </div>
+      <Box
+          className={['App__HBox', className].join(' ')}
+          {...props}
+      >
+        {children}
+      </Box>
   )
 }
 
-HBox.propTypes = {
+VBox.propTypes = {
   className: PropTypes.string,
   children: PropTypes.any,
   props: PropTypes.any
--- src/Layouts/VBox/VBox.tsx
+++ src/Layouts/VBox/VBox.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import './VBox.scss'
+import { Box } from '../Box/Box'
 
 export function VBox ({ children, className, ...props }: InferProps<typeof VBox.propTypes>) {
   return (
-    <div
-        className={['Appl__ContextContent', className].join(' ')}
-        {...props}
+    <Box
+      className={['App__Vbox', className].join(' ')}
+      {...props}
     >
       {children}
-    </div>
+    </Box>
   )
 }
 
