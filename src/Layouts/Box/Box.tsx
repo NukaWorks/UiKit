c295215fa862e95311048022154c4895b471022c
Powerm1nt <nukaworks@nuka.works>
2022-11-16 16:34:43 +0100

    :sparkles: Added base component Box layout

---
+++ src/Layouts/Box/Box.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes, { InferProps } from 'prop-types'
+import './Box.scss'
+
+export function Box ({ children, className, ...props }: InferProps<typeof Box.propTypes>) {
+  return (
+    <div
+        className={['Appl__Box', className].join(' ')}
+        {...props}
+    >
+      {children}
+    </div>
+  )
+}
+
+Box.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.any,
+  props: PropTypes.any
+}
