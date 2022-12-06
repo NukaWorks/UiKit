f831938f2ed525b117779db2510edf9efc64c060
Chabot Leonard <nukaworks@nuka.works>
2022-12-06 21:17:04 +0100

    :construction: Impl SidebarContext

--- src/Base/Sidebar/Sidebar.tsx
+++ src/Base/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import PropTypes, { InferProps } from 'prop-types'
 import './Sidebar.scss'
 import '../../Assets/Themes/index.scss'
 import { SidebarItem } from '../SidebarItem/SidebarItem'
+import SidebarContext from './SidebarContext'
 
 export function Sidebar ({ children, className, ...props }: InferProps<typeof Sidebar.propTypes>) {
   const [close, setClose] = useState(false)
@@ -10,16 +11,18 @@ export function Sidebar ({ children, className, ...props }: InferProps<typeof Si
   const handleToggle = () => setClose(!close)
 
   return (
-    <div
-        className={['Base__Sidebar', close ? 'Base__Sidebar--Closed' : '', className].join(' ')}
-        {...props}
-    >
-      <SidebarItem className="Base__Sidebar--MenuBtn" icon={'menu'} disableText onClick={() => handleToggle()}/>
+    <SidebarContext.Provider value={{ close, handleToggle }}>
+      <div
+          className={['Base__Sidebar', close ? 'Base__Sidebar--Closed' : '', className].join(' ')}
+          {...props}
+      >
+        <SidebarItem className="Base__Sidebar--MenuBtn" icon={'menu'} disableText onClick={() => handleToggle()}/>
 
-      <div className={'Base__Sidebar--Content'}>
-        {children || <SidebarItem text={'Sidebar Empty'} icon={'warning'} />}
+        <div className={'Base__Sidebar--Content'}>
+          {children || <SidebarItem text={'Sidebar Empty'} icon={'warning'} />}
+        </div>
       </div>
-    </div>
+    </SidebarContext.Provider>
   )
 }
 
