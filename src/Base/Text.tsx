015c39e3dbf2f7c6aa218a1d35cc2505d560eb07
Chabot Leonard <nukaworks@nuka.works>
2022-12-23 16:22:34 +0100

    :sparkles: Impl color prop

--- src/Base/Text.tsx
+++ src/Base/Text.tsx
@@ -8,9 +8,9 @@ import styled from 'styled-components'
 const TextElement = styled.div`
   font-family: "Outfit", sans-serif;
   font-size: ${({ size }) => size ? size + 'pt' : '9pt'};
-  color: ${props => (props.disabled ? '#bababa' : 'white')};`
+  color: ${props => (props.disabled ? '#bababa' : props.color || 'black')};`
 
-export function Text ({ className, text, disabled, size, children, ...props } :InferProps<typeof Text.propTypes>) {
+export function Text ({ className, text, disabled, color, size, children, ...props } :InferProps<typeof Text.propTypes>) {
   const [disable, setDisable] = React.useState(false)
 
   useEffect(() => {
@@ -25,6 +25,7 @@ export function Text ({ className, text, disabled, size, children, ...props } :I
         className={['Base__Text', 'Text', disable ? 'Base__Text--Disabled' : '', className].join(' ')}
         disabled={disable}
         size={size}
+        color={color}
         {...props}
     >
         { text || children }
@@ -34,6 +35,7 @@ export function Text ({ className, text, disabled, size, children, ...props } :I
 
 Text.propTypes = {
   text: PropTypes.string,
+  color: PropTypes.string,
   size: PropTypes.number,
   disabled: PropTypes.bool,
   className: PropTypes.string,
