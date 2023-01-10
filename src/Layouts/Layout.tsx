4c0f0de95da2356b7a82da1e69fddaeb165b6f1a
powerm1nt <nukaworks@nuka.works>
2023-01-10 20:21:52 +0100

    :sparkles: Impl scrollable containers

--- src/Layouts/Layout.tsx
+++ src/Layouts/Layout.tsx
@@ -1,12 +1,15 @@
+// @ts-nocheck
+
 import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import styled from 'styled-components'
 
 const LayoutElement = styled.div`
   display: block;
+  overflow: ${({ scrollable }) => scrollable ? 'scroll' : 'hidden'};
 `
 
-export function Layout ({ children, className, ...props }: InferProps<typeof Layout.propTypes>) {
+export function Layout ({ children, className, scrollable, ...props }: InferProps<typeof Layout.propTypes>) {
   return (
     <LayoutElement
         className={['Layouts__Layout', 'Layout', className].join(' ')}
@@ -20,5 +23,6 @@ export function Layout ({ children, className, ...props }: InferProps<typeof Lay
 Layout.propTypes = {
   className: PropTypes.string,
   children: PropTypes.any,
+  scrollable: PropTypes.bool,
   props: PropTypes.any
 }
