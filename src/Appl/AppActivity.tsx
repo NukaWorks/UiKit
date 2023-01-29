05a108406cd65ec82ccbd143b944aac0e1cdc838
powerm1nt <nukaworks@nuka.works>
2023-01-29 19:02:59 +0100

    :children_crossing: Improve AppCativity layout

--- src/Appl/AppActivity.tsx
+++ src/Appl/AppActivity.tsx
@@ -5,21 +5,29 @@ import PropTypes, { InferProps } from 'prop-types'
  * use "import '@powerws/uikit/styling'" to import css in production environment.
  */
 import '../Common/Assets/Themes/index.scss'
+import styled from 'styled-components'
+import { FlexLayout } from '../Layouts/FlexLayout'
 
-export function AppActivity ({ children, className, theme, ...props } :InferProps<typeof AppActivity.propTypes>) {
+const AppActivityElement = styled(FlexLayout)`
+  min-height: 100vh;
+`
+
+export function AppActivity ({ children, direction, className, theme, ...props } :InferProps<typeof AppActivity.propTypes>) {
   return (
-    <div
-        className={[`App__${theme}`, 'Appl__Application', 'AppActivity', className].join(' ')}
+    <AppActivityElement
+        className={[`App__${theme}`, 'Appl__AppActivity', 'AppActivity', className].join(' ')}
+        direction={direction}
         {...props}
     >
         { children }
-    </div>
+    </AppActivityElement>
   )
 }
 
 AppActivity.propTypes = {
   className: PropTypes.string,
   children: PropTypes.any,
+  direction: PropTypes.oneOf(['Vertical', 'Horizontal']),
   theme: PropTypes.oneOf(['Light', 'Dark']),
   props: PropTypes.any
 }
