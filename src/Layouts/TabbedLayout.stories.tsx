1366ea19c2aa57cada4cc9d1c49ab610ed0f1fa0
powerm1nt <nukaworks@nuka.works>
2023-02-07 00:44:06 +0100

    :construction: Wrap into a StackLayout

--- src/Layouts/TabbedLayout.stories.tsx
+++ src/Layouts/TabbedLayout.stories.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
-
 import { TabbedLayout } from './TabbedLayout'
 import { TabPane } from './TabPane'
+import { StackLayout } from './StackLayout'
+import { TabBar } from '../Appl/TabBar'
 
 export default {
   title: 'Layouts/TabbedLayout',
   component: TabbedLayout
 }
 
-const Template = (args: any) => (
-  <TabbedLayout defaultActiveKey={'1'} {...args}>
-    <TabPane name={'Tab 1'} id={'1'}>Test</TabPane>
-    <TabPane name={'Tab 2'} id={'1'}>Lol</TabPane>
-  </TabbedLayout>
-)
-
+const Template = (args: any) => {
+  return (
+      <StackLayout direction={'Vertical'}>
+        <TabBar/>
+        <TabbedLayout defaultActiveKey={'1'} {...args}>
+          <TabPane name={'Tab 1'} tabKey={'1'}>Test</TabPane>
+          <TabPane name={'Tab 2'} tabKey={'2'}>Lol</TabPane>
+        </TabbedLayout>
+      </StackLayout>
+  )
+}
 
 export const Default = Template.bind({})
 // @ts-ignore
