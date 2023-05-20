825e0f4c2583c113040467b1c6e6c2575a13d16b
powerm1nt <nukaworks@nuka.works>
2023-05-20 12:28:31 +0200

    :sparkles: Migrated storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,11 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-scss",
-    "@storybook/addon-postcss",
-    "@storybook/addon-interactions",
-    "storybook-dark-mode"
-  ],
-  "framework": "@storybook/react"
-}
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-scss", "@storybook/addon-postcss", "@storybook/addon-interactions", "storybook-dark-mode", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
