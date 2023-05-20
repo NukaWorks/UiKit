fcaf930a1b7af9fd0ad3eaaa5600efa39f7a5372
powerm1nt <nukaworks@nuka.works>
2023-05-20 12:28:26 +0200

    :sparkles: Using lodash instead of zerodep

--- büng/src/Utils/CompntMgmt.js
+++ büng/src/Utils/CompntMgmt.js
@@ -4,7 +4,7 @@ const {
   getBungFile,
   getTemplateDir
 } = require('./Config')
-const { stringUpperFirst } = require('@zerodep/string')
+const _ = require('lodash-es')
 const bungDirs = new Map()
 const path = require('path')
 
@@ -38,8 +38,8 @@ async function checkProject (name, category) {
       } else {
         console.log(chalk.green(`${chalk.bold('[config]')} Found ${chalk.bold(category)} for ${chalk.bold(bungDirs.get(category))}`))
 
-        name = stringUpperFirst(name)
-        category = stringUpperFirst(category)
+        name = _.upperFirst(name)
+        category = _.upperFirst(category)
 
         return Array.from(srcCategory).forEach(projectFiles => {
           if (!fs.existsSync(projectFiles)) fs.mkdirSync(projectFiles) // Create category folder
@@ -57,11 +57,11 @@ async function checkProject (name, category) {
 }
 
 function patchAndCopy (file, name, category, projectFiles, regex) {
-  name = stringUpperFirst(name) // Capitalize name
+  name = _.upperFirst(name) // Capitalize name
   const patch = regexFile(path.join(getTemplateDir, file), name, category) // Patch the file
 
   if (!regex || file.match(regex)) {
-    file = file.replace(/test/ig, stringUpperFirst(name)) // Rename file
+    file = file.replace(/test/ig, _.upperFirst(name)) // Rename file
     if (fs.existsSync(path.join(projectFiles[0], file))) throw new Error(chalk.red(`${chalk.bold('[project]')} ${chalk.bold(name)} already exists on ${chalk.bold(category)}`))
     console.log(chalk.cyan(`${chalk.bold('[project]')} Making a new element on ${chalk.bold(category)}... (${chalk.bold(file)})`))
 
