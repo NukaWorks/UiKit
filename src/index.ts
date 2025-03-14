50aaef72283e3a6a305ea4344820547dcff0e977
powerm1nt <leonard.chabot@nuka.works>
2025-03-15 00:40:43 +0100

    Export IconButton

--- src/index.ts
+++ src/index.ts
@@ -2,66 +2,68 @@
 import './Common/fonts.scss'
 
 // Base Components
-import { Button } from "./Base/Button";
-import { Text } from "./Base/Text";
-import { TextField } from "./Base/TextField";
-import { MenuList } from "./Base/MenuList";
-import { MenuItem } from "./Base/MenuItem";
-import { MenuBar } from "./Base/MenuBar";
-import { Menu } from "./Base/Menu";
-import { Link } from "./Base/Link";
-import { Sidebar } from "./Base/Sidebar";
-import { SidebarItem } from "./Base/SidebarItem";
-import { ListView } from "./Base/ListView";
+import {Button} from "./Base/Button";
+import {IconButton} from "./Base/IconButton";
+import {Text} from "./Base/Text";
+import {TextField} from "./Base/TextField";
+import {MenuList} from "./Base/MenuList";
+import {MenuItem} from "./Base/MenuItem";
+import {MenuBar} from "./Base/MenuBar";
+import {Menu} from "./Base/Menu";
+import {Link} from "./Base/Link";
+import {Sidebar} from "./Base/Sidebar";
+import {SidebarItem} from "./Base/SidebarItem";
+import {ListView} from "./Base/ListView";
 
 // Application Components
-import { AppHeader } from "./App/AppHeader";
-import { UiApp } from "./App/UiApp";
-import { Dialog } from "./App/Dialog";
+import {AppHeader} from "./App/AppHeader";
+import {UiApp} from "./App/UiApp";
+import {Dialog} from "./App/Dialog";
 
 // Layouts Components
-import { StackLayout } from "./Layouts/StackLayout";
-import { FlexLayout } from "./Layouts/FlexLayout";
-import { Layout } from "./Layouts/Layout";
-import { ScrollLayout } from "./Layouts/ScrollLayout";
-import { Tabs } from "./Layouts/Tabs";
-import { Tab } from "./Layouts/Tab";
-import { TabPanel } from "./Layouts/TabPanel";
-import { TabList } from "./Layouts/TabList";
-import { UncontrolledTabs } from "./Layouts/UncontrolledTabs";
+import {StackLayout} from "./Layouts/StackLayout";
+import {FlexLayout} from "./Layouts/FlexLayout";
+import {Layout} from "./Layouts/Layout";
+import {ScrollLayout} from "./Layouts/ScrollLayout";
+import {Tabs} from "./Layouts/Tabs";
+import {Tab} from "./Layouts/Tab";
+import {TabPanel} from "./Layouts/TabPanel";
+import {TabList} from "./Layouts/TabList";
+import {UncontrolledTabs} from "./Layouts/UncontrolledTabs";
 
 // Miscellaneous Components
-import { Spinner } from "./Misc/Spinner";
-import { Separator } from "./Misc/Separator";
+import {Spinner} from "./Misc/Spinner";
+import {Separator} from "./Misc/Separator";
 
 // Layouts
-import { AppActivity } from "./App/AppActivity";
+import {AppActivity} from "./App/AppActivity";
 
 export {
-  Button,
-  Text,
-  TextField,
-  FlexLayout,
-  AppHeader,
-  UiApp,
-  Menu,
-  MenuList,
-  MenuItem,
-  ListView,
-  MenuBar,
-  Link,
-  AppActivity,
-  Spinner,
-  Separator,
-  Sidebar,
-  ScrollLayout,
-  SidebarItem,
-  StackLayout,
-  UncontrolledTabs,
-  TabPanel,
-  TabList,
-  Tabs,
-  Tab,
-  Layout,
-  Dialog,
+    Button,
+    IconButton,
+    Text,
+    TextField,
+    FlexLayout,
+    AppHeader,
+    UiApp,
+    Menu,
+    MenuList,
+    MenuItem,
+    ListView,
+    MenuBar,
+    Link,
+    AppActivity,
+    Spinner,
+    Separator,
+    Sidebar,
+    ScrollLayout,
+    SidebarItem,
+    StackLayout,
+    UncontrolledTabs,
+    TabPanel,
+    TabList,
+    Tabs,
+    Tab,
+    Layout,
+    Dialog,
 };
