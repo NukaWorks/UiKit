e5a2e37930f7e011d769cbbae86de4405bd15ead
Powerm1nt <leonard.chabot@nuka.works>
2025-04-02 00:54:05 +0200

    Refactor imports for consistency and formatting in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,80 @@
+import { Checkbox } from "./Base/Checkbox";
 // Styling
-import './Common/fonts.scss'
+import "./Common/fonts.scss";
 
 // Base Components
-import {Button} from "./Base/Button";
-import {IconButton} from "./Base/IconButton";
-import {Text} from "./Base/Text";
-import {TextField} from "./Base/TextField";
-import {MenuList} from "./Base/MenuList";
-import {MenuItem} from "./Base/MenuItem";
-import {MenuBar} from "./Base/MenuBar";
-import {Menu} from "./Base/Menu";
-import {Link} from "./Base/Link";
-import {Sidebar} from "./Base/Sidebar";
-import {SidebarItem} from "./Base/SidebarItem";
-import {ListView} from "./Base/ListView";
-import {BubbleContainer} from "./Base/BubbleContainer"
-import {Avatar} from "./Base/Avatar"
+import { Button } from "./Base/Button";
+import { IconButton } from "./Base/IconButton";
+import { Text } from "./Base/Text";
+import { TextField } from "./Base/TextField";
+import { MenuList } from "./Base/MenuList";
+import { MenuItem } from "./Base/MenuItem";
+import { MenuBar } from "./Base/MenuBar";
+import { Menu } from "./Base/Menu";
+import { Link } from "./Base/Link";
+import { Sidebar } from "./Base/Sidebar";
+import { SidebarItem } from "./Base/SidebarItem";
+import { ListView } from "./Base/ListView";
+import { BubbleContainer } from "./Base/BubbleContainer";
+import { Avatar } from "./Base/Avatar";
 
 // Application Components
-import {AppHeader} from "./App/AppHeader";
-import {UiApp} from "./App/UiApp";
-import {Dialog} from "./App/Dialog";
+import { AppHeader } from "./App/AppHeader";
+import { UiApp } from "./App/UiApp";
+import { Dialog } from "./App/Dialog";
 
 // Layouts Components
-import {StackLayout} from "./Layouts/StackLayout";
-import {FlexLayout} from "./Layouts/FlexLayout";
-import {Layout} from "./Layouts/Layout";
-import {ScrollLayout} from "./Layouts/ScrollLayout";
-import {Tabs} from "./Layouts/Tabs";
-import {Tab} from "./Layouts/Tab";
-import {TabPanel} from "./Layouts/TabPanel";
-import {TabList} from "./Layouts/TabList";
-import {UncontrolledTabs} from "./Layouts/UncontrolledTabs";
+import { StackLayout } from "./Layouts/StackLayout";
+import { FlexLayout } from "./Layouts/FlexLayout";
+import { Layout } from "./Layouts/Layout";
+import { ScrollLayout } from "./Layouts/ScrollLayout";
+import { Tabs } from "./Layouts/Tabs";
+import { Tab } from "./Layouts/Tab";
+import { TabPanel } from "./Layouts/TabPanel";
+import { TabList } from "./Layouts/TabList";
+import { UncontrolledTabs } from "./Layouts/UncontrolledTabs";
 
 // Miscellaneous Components
-import {Spinner} from "./Misc/Spinner";
-import {Separator} from "./Misc/Separator";
+import { Spinner } from "./Misc/Spinner";
+import { Separator } from "./Misc/Separator";
 
 // Layouts
-import {AppActivity} from "./App/AppActivity";
+import { AppActivity } from "./App/AppActivity";
 
 // Types
-import {CommonTypes, ComponentBaseProps} from "./Common/Interfaces";
+import { CommonTypes, ComponentBaseProps } from "./Common/Interfaces";
 
 export {
-    Button,
-    IconButton,
-    Text,
-    TextField,
-    FlexLayout,
-    AppHeader,
-    UiApp,
-    Menu,
-    MenuList,
-    MenuItem,
-    ListView,
-    MenuBar,
-    Link,
-    AppActivity,
-    Spinner,
-    Separator,
-    Sidebar,
-    ScrollLayout,
-    SidebarItem,
-    StackLayout,
-    UncontrolledTabs,
-    TabPanel,
-    TabList,
-    Tabs,
-    Tab,
-    Layout,
-    Dialog,
-    Avatar,
-    BubbleContainer,
+  Button,
+  IconButton,
+  Text,
+  TextField,
+  FlexLayout,
+  AppHeader,
+  UiApp,
+  Menu,
+  MenuList,
+  MenuItem,
+  ListView,
+  MenuBar,
+  Link,
+  AppActivity,
+  Spinner,
+  Separator,
+  Sidebar,
+  ScrollLayout,
+  SidebarItem,
+  StackLayout,
+  UncontrolledTabs,
+  TabPanel,
+  TabList,
+  Tabs,
+  Tab,
+  Layout,
+  Dialog,
+  Avatar,
+  BubbleContainer,
+  Checkbox,
 };
 
-export type {
-    CommonTypes,
-    ComponentBaseProps,
-};
+export type { CommonTypes, ComponentBaseProps };
